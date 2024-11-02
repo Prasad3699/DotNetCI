55429c22b48652bfd4b18a7f1a60367a26226eb7
Venkata Vara Prasad Emandi <prasad.emandi369@gmail.com>
2024-11-02 16:33:27 +0530

    k8s testing

---
+++ TestApp/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("helloworld")] 
+        public IActionResult GetHelloWorld()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
